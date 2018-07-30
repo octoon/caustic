295bea552a29c66b3360a6708fc894bcb5869f94
JunWang <1103870790@qq.com>
2018-07-30 13:07:30 +0800

    attach shape

--- source/octoon-ray/main.cpp
+++ source/octoon-ray/main.cpp
@@ -12,7 +12,7 @@ int main()
 		DeviceInfo devinfo;
 		IntersectionApi::GetDeviceInfo(idx, devinfo);
 
-		if (devinfo.type == DeviceInfo::kGpu && devinfo.platform == DeviceInfo::kOpenCL)
+		if (devinfo.type == DeviceInfo::kGpu /*&& devinfo.platform == DeviceInfo::kOpenCL*/)
 		{
 			deviceidx = idx;
 		}
@@ -22,6 +22,31 @@ int main()
 
 	IntersectionApi* api = IntersectionApi::Create(deviceidx);
 
+	// Mesh vertices
+	float const g_vertices[] = {
+		-1.f,-1.f,0.f,
+		1.f,-1.f,0.f,
+		0.f,1.f,0.f,
+	};
+	int const g_indices[] = { 0, 1, 2 };
+	// Number of vertices for the face
+	const int g_numfaceverts[] = { 3 };
+
+	Shape* shape = api->CreateMesh(g_vertices, 3, 3 * sizeof(float), g_indices, 0, g_numfaceverts, 1);
+
+	api->AttachShape(shape);
+	//api->DetachShape(shape);
+
+	ray rays[3];
+	rays[0].o = float4(0.f, 0.f, -1.f, 1000.f);
+	rays[0].d = float3(0.f, 0.f, 10.f);
+	rays[1].o = float4(0.f, 0.5f, -10.f, 1000.f);
+	rays[1].d = float3(0.f, 0.f, 1.f);
+	rays[2].o = float4(0.4f, 0.f, -10.f, 1000.f);
+	rays[2].d = float3(0.f, 0.f, 1.f);
+	Buffer* ray_buffer = api->CreateBuffer(3 * sizeof(ray), rays);
+
+	Buffer* isect_buffer = api->CreateBuffer(3 * sizeof(Intersection), nullptr);
 
     return 0;
 }
