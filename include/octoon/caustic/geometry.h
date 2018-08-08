110663eb06833c70a7dcf83b4575e24a6aa5e121
Rui <2221870259@qq.com>
2018-08-08 10:00:04 +0800

    update

--- include/octoon/caustic/geometry.h
+++ include/octoon/caustic/geometry.h
@@ -13,6 +13,9 @@ namespace octoon
 			Geometry() noexcept;
 			virtual ~Geometry() noexcept;
 
+			void setTransform(const float m[4][4], const float minv[4][4]) noexcept;
+			void getTransform(const float m[4][4], const float minv[4][4]) const noexcept;
+
 		private:
 			Geometry(const Geometry&) noexcept = delete;
 			Geometry& operator=(const Geometry&) noexcept = delete;
