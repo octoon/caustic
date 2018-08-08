6ad14a77317b7d80a54a20a31d62f387a82b3dcf
Rui <2221870259@qq.com>
2018-08-08 11:00:01 +0800

    update

--- include/octoon/caustic/sampler.h
+++ include/octoon/caustic/sampler.h
@@ -2,9 +2,7 @@
 #define OCTOON_CAUSTIC_SAMPLER
 
 #include <algorithm>
-
-#include "math.h"
-#include "texture.h"
+#include <octoon/caustic/texture.h>
 
 namespace octoon
 {
@@ -14,37 +12,116 @@ namespace octoon
         class Sampler
         {
         public:
-            virtual T sample(const Texture<T> &origin, float u, float lod) = 0;
-            virtual T sample(const Texture<T> &origin, float u, float v, float lod) = 0;
-            virtual T sample(const Texture<T> &origin, float u, float v, float w, float lod) = 0;
+			enum class Wrap : std::uint8_t
+			{
+				Repeat = 1,
+				Mirror = 2,
+				ClampToEdge = 3,
+			};
+
+			enum class Filter : std::uint8_t
+			{
+				Nearest = 0,
+				Linear = 1,
+				NearestMipmapLinear = 2,
+				NearestMipmapNearest = 3,
+				LinearMipmapNearest = 4,
+				LinearMipmapLinear = 5,
+				Anisotropic = 6
+			};
+
+		public:
+			Sampler() noexcept : wrapMode_(Wrap::ClampToEdge) {}
+			virtual ~Sampler() noexcept {}
+
+			void setWrapMode(Wrap mode) { wrapMode_ = mode; }
+			Wrap getWrapMode() const { return wrapMode_; }
+
+		public:
+            virtual T sample(const Texture<T>& texels, float u, float lod) = 0;
+            virtual T sample(const Texture<T>& texels, float u, float v, float lod) = 0;
+            virtual T sample(const Texture<T>& texels, float u, float v, float w, float lod) = 0;
+
+		private:
+			Wrap wrapMode_;
         };
 
         template<class T>
         class NearestNeighborInterpolation : public Sampler<T>
         {
         public:
-            virtual T sample(const Texture<T> &origin, float u, float lod)
+            virtual T sample(const Texture<T>& texels, float u, float lod)
             {
 
             }
-            virtual T sample(const Texture<T> &origin, float u, float v, float lod)
+
+            virtual T sample(const Texture<T>& texels, float u, float v, float lod)
             {
-                auto x = clamp(u, 0.f, 1.f);
-                auto y = clamp(v, 0.f, 1.f);
-                auto n = origin.evaluate((int)std::round(x), (int)std::round(y));
+				switch (this->getWrapMode())
+				{
+				case Wrap::Repeat:
+				{
+					auto x = u - std::floor(u);
+					auto y = v - std::floor(v);
+					return texels.fetch((int)std::round(x), (int)std::round(y));
+				}
+				break;
+				case Wrap::Mirror:
+				{
+					auto iu = ((std::uint32_t)std::floor(u));
+					auto iv = ((std::uint32_t)std::floor(v));
+					auto x = (iu % 2) > 0 ? 1.0f - (u - iu) : (u - iu);
+					auto y = (iv % 2) > 0 ? 1.0f - (v - iv) : (v - iv);
+					return texels.fetch((int)std::round(x), (int)std::round(y));
+				}
+				break;
+				case Wrap::ClampToEdge:
+				{
+					auto x = clamp(u, 0.f, 1.f);
+					auto y = clamp(v, 0.f, 1.f);
+					return texels.fetch((int)std::round(x), (int)std::round(y));
+				}
+				break;
+				}
 
                 return n;
             }
-            virtual T sample(const Texture<T> &origin, float u, float v, float w, float lod)
-            {
-                auto x = clamp(u, 0.f, 1.f);
-                auto y = clamp(v, 0.f, 1.f);
-                auto z = clamp(w, 0.f, 1.f);
-                auto n = origin.evaluate((int)std::round(x), (int)std::round(y), (int)std::round(z));
 
-                return n;
+            virtual T sample(const Texture<T>& texels, float u, float v, float w, float lod)
+            {
+				switch (this->getWrapMode())
+				{
+				case Wrap::Repeat:
+				{
+					auto x = u - std::floor(u);
+					auto y = v - std::floor(v);
+					auto z = z - std::floor(z);
+					return texels.fetch((int)std::round(x), (int)std::round(y), (int)std::round(z));
+				}
+				break;
+				case Wrap::Mirror:
+				{
+					auto iu = ((std::uint32_t)std::floor(u));
+					auto iv = ((std::uint32_t)std::floor(v));
+					auto iw = ((std::uint32_t)std::floor(w));
+					auto x = (iu % 2) > 0 ? 1.0f - (u - iu) : (u - iu);
+					auto y = (iv % 2) > 0 ? 1.0f - (v - iv) : (v - iv);
+					auto z = (iw % 2) > 0 ? 1.0f - (w - iv) : (w - iv);
+					return texels.fetch((int)std::round(x), (int)std::round(y), (int)std::round(z));
+				}
+				break;
+				case Wrap::ClampToEdge:
+				{
+					auto x = clamp(u, 0.f, 1.f);
+					auto y = clamp(v, 0.f, 1.f);
+					auto z = clamp(w, 0.f, 1.f);
+					return texels.fetch((int)std::round(x), (int)std::round(y), (int)std::round(z));
+				}
+				break;
+				}
             }
         };
     }
-} // namespace octoon
-#endif // OCTOON_CAUSTIC_SAMPLER
+}
+
+#endif
--- include/octoon/caustic/texture.h
+++ include/octoon/caustic/texture.h
@@ -5,24 +5,40 @@
 
 namespace octoon
 {
-    template<class T>
-    class Texture
-    {
-    public:
-        virtual T evaluate(float u, float v, float w) const = 0;
-        virtual ~Texture() = default;
-    };
+	namespace caustic
+	{
+		template<class T>
+		class Texture
+		{
+		public:
+			enum Type
+			{
+				Texture2D = 0,
+				Texture2DArray = 1,
+				Texture3D = 2,
+				Cube = 3,
+				CubeArray = 4,
+				Latlong = 5, // environment hdri
+				FishEye
+			};
 
-    template<class T>
-    class ConstantTexture : public Texture<T>
-    {
-    public:
-        ConstantTexture(const T &value) : value(value) {}
-        virtual ~ConstantTexture() = default;
-        virtual T evaluate(float u, float v, float w) const override { return value; }
+		public:
+			virtual ~Texture() = default;
+			virtual T fetch(float u, float v, float w) const = 0;
+		};
 
-    private:
-        T value;
-    };
-} // namespace octoon
+		template<class T>
+		class ConstantTexture : public Texture<T>
+		{
+		public:
+			ConstantTexture(const T &value) : value(value) {}
+			virtual ~ConstantTexture() = default;
+			virtual T fetch(float u, float v, float w) const override { return value; }
+
+		private:
+			T value;
+		};
+	}
+}
+
 #endif
